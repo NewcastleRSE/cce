24ca59ee534a67fe4434ec5c94aaaf3186d087fa
Robin Nandi <robin.nandi@newcastle.ac.uk>
2022-11-29 10:26:54 +0000

    changes for trial azure deployment

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -7,6 +7,7 @@ from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+from apps.authentication.models import Users
 
 
 @blueprint.route('/index')
@@ -28,8 +29,10 @@ def route_template(template):
         # Detect the current page
         segment = get_segment(request)
 
+        data = Users.query.all()
+
         # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment)
+        return render_template("home/" + template, segment=segment, data=data)
 
     except TemplateNotFound:
         return render_template('home/page-404.html'), 404
